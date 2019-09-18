54f8000c00b05b84794a80fe5807a011cd3882dc
mesh1nek0x0 <mesh1nek0x0@gmail.com>
2019-09-18 19:00:09 +0900

    :shirt: apply lint

--- helper.js
+++ helper.js
@@ -1,16 +1,17 @@
-const axios = require('axios');
+const axios = require("axios");
 
 module.exports = class Helper {
-    getData(param) {
-        return axios.get('http://dummy.restapiexample.com/api/v1/employee/42932')
-        .then(function (response) {
-            // handle success
-            return { code: 0, body: response.data };
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-            return {code: 1, bode: error.message }
-        })
-    }
-}
+  getData() {
+    return axios
+      .get("http://dummy.restapiexample.com/api/v1/employee/42932")
+      .then(function(response) {
+        // handle success
+        return { code: 0, body: response.data };
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+        return { code: 1, bode: error.message };
+      });
+  }
+};
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const Helper = require('./helper');
+const Helper = require("./helper");
 
 async function func1() {
-    const helper = new Helper();
-    const result = await helper.getData({});
-    console.log(result);
-    return result;
-    
-};
+  const helper = new Helper();
+  const result = await helper.getData();
+  console.log(result);
+  return result;
+}
 
 func1();
